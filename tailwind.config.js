4db5585e4fdd96acb04de353772af62ab7e21b5c
John Torres <apodpm@gmail.com>
2022-05-04 22:38:22 +0800

    init nuxt.js v3

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: defaultTheme.colors.green
+            }
+        }
+    },
+    content: [
+        `components/**/*.{vue,js}`,
+        `layouts/**/*.vue`,
+        `pages/**/*.vue`,
+        `composables/**/*.{js,ts}`,
+        `plugins/**/*.{js,ts}`,
+        `App.{js,ts,vue}`,
+        `app.{js,ts,vue}`
+    ]
+}
