7082665f82bdd85f7f2689f6b0e34fba6ba9b748
John Torres <apodpm@gmail.com>
2022-05-10 06:01:29 +0800

    update: /gas

--- pages/gas.vue
+++ pages/gas.vue
@@ -23,101 +23,168 @@ onMounted(async () => {
 
 
 <template>
-  <LAutoWidth class="py-8">
-    <div class="font-mono"></div>
-
-    <div class="mt-6 px-4 sm:px-6 lg:px-8">
-      <div class="sm:flex sm:items-center">
-        <div class="sm:flex-auto">
-          <h1 class="text-xl font-semibold text-gray-900">
-            {{ transactions.length ? transactions.length : '___' }} Transactions
-          </h1>
-          <p class="mt-2 text-sm text-gray-700">
-            Pending transactions in next block #{{ blockNumber ? blockNumber : '___' }}
+  <div>
+    <div class="bg-indigo-800">
+      <div
+        class="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20"
+      >
+        <div class="max-w-4xl mx-auto text-center">
+          <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+            Aggressive Gas Price Monitor
+          </h2>
+          <p class="mt-3 text-xl text-indigo-200 sm:mt-4">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Repellendus repellat laudantium.
           </p>
         </div>
-      </div>
-      <div class="mt-8 flex flex-col">
-        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div
-            class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
-          >
-            <div
-              class="
-                overflow-hidden
-                shadow
-                ring-1 ring-black ring-opacity-5
-                md:rounded-lg
-              "
+        <dl
+          class="
+            mt-10
+            text-center
+            sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8
+          "
+        >
+          <div class="flex flex-col">
+            <dt
+              class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
             >
-              <table
-                class="min-w-full divide-y divide-gray-300 ac-table-s font-mono"
+              Pepperoni
+            </dt>
+            <dd class="order-1 text-5xl font-extrabold text-white">100%</dd>
+          </div>
+          <div class="flex flex-col mt-10 sm:mt-0">
+            <dt
+              class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
+            >
+              Delivery
+            </dt>
+            <dd class="order-1 text-5xl font-extrabold text-white">24/7</dd>
+          </div>
+          <div class="flex flex-col mt-10 sm:mt-0">
+            <dt
+              class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
+            >
+              Calories
+            </dt>
+            <dd class="order-1 text-5xl font-extrabold text-white">100k+</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+
+    <LAutoWidth class="py-8">
+      <div class="font-mono"></div>
+
+      <div class="mt-6 px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+          <div class="sm:flex-auto">
+            <h1 class="text-xl font-semibold text-gray-900">
+              {{ transactions.length ? transactions.length : "___" }}
+              Transactions
+            </h1>
+            <p class="mt-2 text-sm text-gray-700">
+              Pending transactions in next block #{{
+                blockNumber ? blockNumber : "___"
+              }}
+            </p>
+          </div>
+        </div>
+        <div class="mt-8 flex flex-col">
+          <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div
+              class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
+            >
+              <div
+                class="
+                  overflow-hidden
+                  shadow
+                  ring-1 ring-black ring-opacity-5
+                  md:rounded-lg
+                "
               >
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th scope="col">Hash</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Gas Price</th>
-                    <th scope="col">Max Fee</th>
-                    <th scope="col">Max Priority Fee</th>
-                    <th scope="col">
-                      <span class="sr-only">Edit</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200 bg-white">
-                  <tr v-for="tx in transactions" :key="tx.hash">
-                    <td
-                      class="
-                        whitespace-nowrap
-                        py-2
-                        pl-4
-                        pr-3
-                        text-sm text-gray-500
-                        sm:pl-6
-                      "
-                    >
-                      {{ tx.hash.slice(0, 10) }}...{{ tx.hash.slice(-4) }}
-                    </td>
-                    <td
-                      class="
-                        whitespace-nowrap
-                        px-2
-                        py-2
-                        text-sm
-                        font-medium
-                        text-gray-900
-                      "
-                    >
-                      {{ tx.type }}
-                    </td>
-                    <td class="text-gray-900">
-                      {{ ethers.utils.formatUnits(tx.gasPrice, "gwei") }}
-                    </td>
-                    <td>
-                      <span v-if="tx.type === 2">
-                        {{ ethers.utils.formatUnits(tx.maxFeePerGas, "gwei") }}
-                      </span>
-                    </td>
-                    <td>
-                      <span v-if="tx.type === 2">
-                        {{ ethers.utils.formatUnits(tx.maxPriorityFeePerGas, "gwei") }}
-                      </span>
-                    </td>
-                    <td>
-                      <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                        >View<span class="sr-only">, {{ tx.hash }}</span></a
+                <table
+                  class="
+                    min-w-full
+                    divide-y divide-gray-300
+                    ac-table-s
+                    font-mono
+                  "
+                >
+                  <thead class="bg-gray-50">
+                    <tr>
+                      <th scope="col">Hash</th>
+                      <th scope="col">Type</th>
+                      <th scope="col">Gas Price</th>
+                      <th scope="col">Max Fee</th>
+                      <th scope="col">Max Priority Fee</th>
+                      <th scope="col">
+                        <span class="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-200 bg-white">
+                    <tr v-for="tx in transactions" :key="tx.hash">
+                      <td
+                        class="
+                          whitespace-nowrap
+                          py-2
+                          pl-4
+                          pr-3
+                          text-sm text-gray-500
+                          sm:pl-6
+                        "
                       >
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+                        {{ tx.hash.slice(0, 10) }}...{{ tx.hash.slice(-4) }}
+                      </td>
+                      <td
+                        class="
+                          whitespace-nowrap
+                          px-2
+                          py-2
+                          text-sm
+                          font-medium
+                          text-gray-900
+                        "
+                      >
+                        {{ tx.type }}
+                      </td>
+                      <td class="text-gray-900">
+                        {{ ethers.utils.formatUnits(tx.gasPrice, "gwei") }}
+                      </td>
+                      <td>
+                        <span v-if="tx.type === 2">
+                          {{
+                            ethers.utils.formatUnits(tx.maxFeePerGas, "gwei")
+                          }}
+                        </span>
+                      </td>
+                      <td>
+                        <span v-if="tx.type === 2">
+                          {{
+                            ethers.utils.formatUnits(
+                              tx.maxPriorityFeePerGas,
+                              "gwei"
+                            )
+                          }}
+                        </span>
+                      </td>
+                      <td>
+                        <a
+                          href="#"
+                          class="text-indigo-600 hover:text-indigo-900"
+                          >View<span class="sr-only">, {{ tx.hash }}</span></a
+                        >
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </LAutoWidth>
+    </LAutoWidth>
+  </div>
 </template>
 
 <style scoped lang="scss">
