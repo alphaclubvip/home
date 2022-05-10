f7ef15861852f3c4d1fdad7e387c1e519e54d742
John Torres <apodpm@gmail.com>
2022-05-11 02:59:20 +0800

    optimized: /gas

--- pages/gas.vue
+++ pages/gas.vue
@@ -1,9 +1,50 @@
 <script setup lang="ts">
-const { $ethers } = useNuxtApp();
+import { ethers, BigNumber } from "ethers";
 
-const ethers = $ethers;
-const transactions = ref([]);
-const blockNumber = ref();
+const nextBlock = ref();
+const pendingTransactions = ref([]);
+const pendingTransactionsByMaxFee = ref([]);
+
+function maxFeePerGasOf(tx: ethers.Transaction) {
+  return tx.type === 2 ? tx.maxFeePerGas : tx.gasPrice;
+}
+
+function maxPriorityFeePerGasOf(tx: ethers.Transaction) {
+  return tx.type === 2
+    ? tx.maxPriorityFeePerGas
+    : tx.gasPrice.sub(nextBlock.value.baseFeePerGas);
+}
+
+function sortTransactionByMaxPriorityFee(
+  a: ethers.Transaction,
+  b: ethers.Transaction
+) {
+  return maxPriorityFeePerGasOf(b).sub(maxPriorityFeePerGasOf(a)).toNumber();
+}
+
+function sortTransactionByMaxFee(a: ethers.Transaction, b: ethers.Transaction) {
+  return maxFeePerGasOf(b).sub(maxFeePerGasOf(a)).toNumber();
+}
+
+const maxFeePerGas = computed(() => {
+  if (!pendingTransactionsByMaxFee.value.length) {
+    return 0;
+  }
+
+  const i = Math.ceil((pendingTransactionsByMaxFee.value.length / 3) * 2);
+
+  return maxFeePerGasOf(pendingTransactionsByMaxFee.value[i]);
+});
+
+const maxPriorityFeePerGas = computed(() => {
+  if (!pendingTransactions.value.length) {
+    return 0;
+  }
+
+  const i = Math.ceil((pendingTransactions.value.length / 3) * 2);
+
+  return maxPriorityFeePerGasOf(pendingTransactions.value[i]);
+});
 
 onMounted(async () => {
   console.log("/gas");
@@ -13,10 +54,15 @@ onMounted(async () => {
       const { $web3 } = useNuxtApp();
       const block = await $web3.getBlockWithTransactions("pending");
 
-      blockNumber.value = block.number;
-      transactions.value = block.transactions;
+      nextBlock.value = block;
+
+      pendingTransactions.value = Array(...block.transactions);
+      pendingTransactions.value.sort(sortTransactionByMaxPriorityFee);
+
+      pendingTransactionsByMaxFee.value = Array(...block.transactions);
+      pendingTransactionsByMaxFee.value.sort(sortTransactionByMaxFee);
     }
-    await new Promise((r) => setTimeout(r, 2000));
+    await new Promise((r) => setTimeout(r, 1500));
   }
 });
 </script>
@@ -33,11 +79,18 @@ onMounted(async () => {
             Aggressive Gas Price Monitor
           </h2>
           <p class="mt-3 text-xl text-indigo-200 sm:mt-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Repellendus repellat laudantium.
+            <template v-if="nextBlock">
+              According to {{ nextBlock.transactions.length }} pending
+              transactions in the next BLOCK#{{ nextBlock.number }}
+            </template>
+            <template v-else>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Repellendus repellat laudantium.
+            </template>
           </p>
         </div>
         <dl
+          v-if="nextBlock"
           class="
             mt-10
             text-center
@@ -48,42 +101,60 @@ onMounted(async () => {
             <dt
               class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
             >
-              Pepperoni
+              Max Priority Fee
             </dt>
-            <dd class="order-1 text-5xl font-extrabold text-white">100%</dd>
+            <dd class="order-1 text-5xl font-extrabold text-white">
+              {{
+                Math.ceil(
+                  ethers.utils.formatUnits(maxPriorityFeePerGas, "gwei")
+                )
+              }}
+            </dd>
           </div>
           <div class="flex flex-col mt-10 sm:mt-0">
             <dt
               class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
             >
-              Delivery
+              Max Fee
             </dt>
-            <dd class="order-1 text-5xl font-extrabold text-white">24/7</dd>
+            <dd class="order-1 text-5xl font-extrabold text-white">
+              {{ Math.ceil(ethers.utils.formatUnits(maxFeePerGas, "gwei")) }}
+            </dd>
           </div>
           <div class="flex flex-col mt-10 sm:mt-0">
             <dt
               class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200"
             >
-              Calories
+              Base Fee
             </dt>
-            <dd class="order-1 text-5xl font-extrabold text-white">100k+</dd>
+            <dd class="order-1 text-5xl font-extrabold text-white">
+              {{
+                Math.ceil(
+                  ethers.utils.formatUnits(nextBlock.baseFeePerGas, "gwei")
+                )
+              }}
+            </dd>
           </div>
         </dl>
       </div>
     </div>
 
-    <LAutoWidth class="py-8" v-if="transactions.length">
+    <LAutoWidth class="py-8" v-if="nextBlock">
       <div class="mt-6 px-4 sm:px-6 lg:px-8">
         <div class="sm:flex sm:items-center">
           <div class="sm:flex-auto">
             <h1 class="text-xl font-semibold text-gray-900">
-              {{ transactions.length ? transactions.length : "___" }}
-              Transactions
+              {{
+                nextBlock.transactions.length
+                  ? nextBlock.transactions.length
+                  : "___"
+              }}
+              Pending Transactions
             </h1>
             <p class="mt-2 text-sm text-gray-700">
-              Pending transactions in next block #{{
-                blockNumber ? blockNumber : "___"
-              }}
+              Pending transactions in next block
+              <template v-if="nextBlock"> #{{ nextBlock.number }} </template>
+              <template v-else> #___ </template>
             </p>
           </div>
         </div>
@@ -112,7 +183,6 @@ onMounted(async () => {
                     <tr>
                       <th scope="col">Hash</th>
                       <th scope="col">Type</th>
-                      <th scope="col">Gas Price</th>
                       <th scope="col">Max Fee</th>
                       <th scope="col">Max Priority Fee</th>
                       <th scope="col">
@@ -121,26 +191,42 @@ onMounted(async () => {
                     </tr>
                   </thead>
                   <tbody class="divide-y divide-gray-200 bg-white">
-                    <tr v-for="tx in transactions" :key="tx.hash">
-                      <td>{{ tx.hash.slice(0, 10) }}...{{ tx.hash.slice(-4) }}</td>
-                      <td>{{ tx.type }}</td>
-                      <td>{{ ethers.utils.formatUnits(tx.gasPrice, "gwei") }}</td>
+                    <tr v-for="tx in pendingTransactions" :key="tx.hash">
                       <td>
-                        <span v-if="tx.type === 2">
+                        {{ tx.hash.slice(0, 10) }}...{{ tx.hash.slice(-4) }}
+                      </td>
+                      <td>
+                        <template v-if="tx.type === 2">
+                          {{ tx.type }}
+                        </template>
+                      </td>
+                      <td>
+                        <template v-if="tx.type === 2">
                           {{
                             ethers.utils.formatUnits(tx.maxFeePerGas, "gwei")
                           }}
-                        </span>
+                        </template>
+                        <template v-else>
+                          {{ ethers.utils.formatUnits(tx.gasPrice, "gwei") }}
+                        </template>
                       </td>
                       <td>
-                        <span v-if="tx.type === 2">
+                        <template v-if="tx.type === 2">
                           {{
                             ethers.utils.formatUnits(
                               tx.maxPriorityFeePerGas,
                               "gwei"
                             )
                           }}
-                        </span>
+                        </template>
+                        <template v-else>
+                          {{
+                            ethers.utils.formatUnits(
+                              tx.gasPrice.sub(nextBlock.baseFeePerGas),
+                              "gwei"
+                            )
+                          }}
+                        </template>
                       </td>
                       <td>
                         <a
@@ -175,15 +261,21 @@ onMounted(async () => {
     }
   }
 
-  tbody tr td {
-    @apply whitespace-nowrap px-2 py-2 text-sm text-gray-900;
+  tbody tr {
+    td {
+      @apply whitespace-nowrap px-2 py-2 text-sm text-gray-900;
 
-    &:first-child {
-      @apply pl-4 pr-3 text-sm text-gray-500 sm:pl-6;
+      &:first-child {
+        @apply pl-4 pr-3 text-sm text-gray-500 sm:pl-6;
+      }
+
+      &:last-child {
+        @apply relative pl-3 pr-4 text-right text-sm font-medium sm:pr-6;
+      }
     }
 
-    &:last-child {
-      @apply relative pl-3 pr-4 text-right text-sm font-medium sm:pr-6;
+    &:nth-child(even) {
+      @apply bg-gray-50;
     }
   }
 }
