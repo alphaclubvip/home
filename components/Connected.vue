50ddeae4be0e99194ed445965e35234648b630ee
John Torres <apodpm@gmail.com>
2022-05-29 05:40:40 +0800

    update: Connected

--- components/Connected.vue
+++ components/Connected.vue
@@ -12,7 +12,7 @@ const account = useWeb3Account();
     <ConnectWallet type="button"
       class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-24 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
       <LockOpenIcon class="mx-auto h-12 w-12 text-gray-400" />
-      <span class="mt-4 block text-sm font-medium text-gray-900"> Connect Wallet to Continue... </span>
+      <span class="mt-4 block font-mono font-medium text-gray-400"> Connect Wallet to Continue... </span>
     </ConnectWallet>
   </LAutoWidth>
 </template>
